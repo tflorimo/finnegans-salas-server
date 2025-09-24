587a8b0022158aef51b56312d1f809183fdfed08
tflorimo <tflorimo@gmail.com>
2025-09-23 22:10:22 -0300

    ts format de room

--- src/models/room.ts
+++ src/models/room.ts
@@ -1,42 +1,44 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database";
 
-const Room = sequelize.define('Room', {
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        primaryKey: true,
-        validate: {
-            isEmail: true,
-        }
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
+const Room = sequelize.define
+    (
+        'Room', {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            primaryKey: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
 
-    },
-    capacity: {  // dato creado para el admin 
-        type: DataTypes.INTEGER,
-        allowNull: false,
-    },
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: true,
+        },
+        capacity: {  // dato creado para el admin 
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: true,
 
 
-    },
-    displayName: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        },
+        displayName: {
+            type: DataTypes.STRING,
+            allowNull: false,
 
-    },
-}, {
-    tableName: 'rooms',  //nombre en la tabla de la bd
-    indexes: [
-        { fields: ['email'] },
-        { fields: ['name'] },
-    ],
-});
+        },
+    }, {
+        tableName: 'rooms',  //nombre en la tabla de la bd
+        indexes: [
+            { fields: ['email'] },
+            { fields: ['name'] },
+        ],
+    });
 
 export default Room;
 
