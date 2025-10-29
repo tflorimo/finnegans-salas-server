37d9af40c45abb207b132f489b16036269d0fab6
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-10-28 22:17:24 -0300

    Se elimina ruta de tokenStatus

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -5,6 +5,5 @@ const router = Router();
 
 router.get('/google', authController.authRedirect);
 router.get('/oauth2callback', authController.oauth2Callback);
-router.get('/token-status', authController.tokenStatus);
 
 export default router;
