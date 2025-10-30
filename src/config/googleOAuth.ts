5c78649cd7a4482b1b74924d643abf9fe6e1b4d1
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-10-30 18:59:47 -0300

    Corrección y simplificación del config de OAuth

--- src/config/googleOAuth.ts
+++ src/config/googleOAuth.ts
@@ -1,12 +1,16 @@
 import { google } from "googleapis";
-import fs from "fs";
-import path from "path";
+import dotenv from 'dotenv';
 
-const CREDENTIALS_PATH = path.join(__dirname, "../../credentials.json");
-const credentials = JSON.parse(fs.readFileSync(CREDENTIALS_PATH, "utf8"));
+dotenv.config();
+
+const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI } = process.env;
+if (!GOOGLE_CLIENT_ID || !GOOGLE_CLIENT_SECRET || !GOOGLE_REDIRECT_URI) {
+  throw new Error("Faltan variables de entorno para OAuth.");
+}
 
 export const oauth2Client = new google.auth.OAuth2(
-  credentials.web.client_id,
-  credentials.web.client_secret,
-  credentials.web.redirect_uris[0]
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  GOOGLE_REDIRECT_URI
 );
+
