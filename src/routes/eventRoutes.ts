71f0340b024f440728a6ed49c4de79ae4723d0ac
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-10-30 15:07:47 -0300

    Add requireAdmin middleware to the getAllEvents route

--- src/routes/eventRoutes.ts
+++ src/routes/eventRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import EventController from '../controllers/eventController';
-import { authenticate } from '../middleware/auth';
+import { authenticate, requireAdmin } from '../middleware/auth';
 
 const router = Router();
 
-router.get('/', authenticate, EventController.getAllEvents);
+router.get('/', authenticate, requireAdmin, EventController.getAllEvents);
 router.get('/:id', authenticate, EventController.getEventById);
-// router.patch('/:id/checkin', authenticate, EventController.checkInEvent);
+// router.patch('/:id/checkin', EventController.checkInEvent);
 
 export default router;
