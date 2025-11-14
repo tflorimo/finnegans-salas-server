31d5a91b9caf6489125fdefc18fdae26118f2e51
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-13 22:45:13 -0300

    Modificación de comentarios

--- src/config/oAuthAccess.ts
+++ src/config/oAuthAccess.ts
@@ -1,5 +1,5 @@
 const allowedDomain = process.env.GOOGLE_ALLOWED_DOMAIN;
-//@TODO: Comentar para producción
+//@TODO: Eliminar para producción
 const testerEmails = new Set(
   (process.env.GOOGLE_TEST_USERS || "")
     .split(",")
@@ -28,7 +28,7 @@ export const ensureOAuthAccess = (email: string): void => {
       throw new OAuthAccessDeniedError("oauth_domain_not_allowed");
     } */
 
-  //@TODO: Comentar para producción
+  //@TODO: Eliminar para producción
   if (!testerEmails.has(normalized)) {
     throw new OAuthAccessDeniedError("oauth_not_tester");
   }
