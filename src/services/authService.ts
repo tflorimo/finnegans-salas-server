d085d5bd3847a51d04eb8852508f4a36fca6fd33
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-24 01:29:15 -0300

    Eliminación del alcance de envío de Gmail (Gmail send scope) de la configuración OAuth en AuthService.

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -13,7 +13,6 @@ class AuthService {
       scope: [
         "https://www.googleapis.com/auth/userinfo.profile",
         "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/gmail.send"
       ],
     });
   }
