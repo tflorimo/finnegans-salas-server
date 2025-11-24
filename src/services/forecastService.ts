0950b71ca4cd169d28a65244275f2b5742c3ac4c
tflorimo <tflorimo@gmail.com>
2025-11-24 17:26:25 -0300

    agregado de roomName a dto

--- src/services/forecastService.ts
+++ src/services/forecastService.ts
@@ -1,5 +1,5 @@
 import { HourlyForecastDTO, RoomHourlyForecastDTO } from "../dtos/forecastDTO";
-import { Forecast } from "../models";
+import { Forecast, Room } from "../models";
 class ForecastService {
     /**
      * Obtiene las predicciones de ocupación horaria para las salas.
@@ -36,12 +36,20 @@ class ForecastService {
 
         const forecasts = await Forecast.findAll({
             where: whereClause,
+            include: [{
+                model: Room,
+                as: 'room',
+                attributes: ['name'] // nombre de la sala
+            }],
             order: [['roomEmail', 'ASC'], ['date', 'ASC']],
         });
 
-        const grouped = new Map<string, HourlyForecastDTO[]>();
+        const grouped = new Map<string, {name: string, list: HourlyForecastDTO[]}>();
 
-        forecasts.forEach((forecast) => {
+        forecasts.forEach((forecast: any) => {
+
+            const currRoomName = forecast.room?.name || "Sala desconocida";
+
             const record: HourlyForecastDTO = {
                 roomEmail: forecast.roomEmail,
                 date: forecast.date,
@@ -51,15 +59,16 @@ class ForecastService {
             };
 
             if (!grouped.has(forecast.roomEmail)) {
-                grouped.set(forecast.roomEmail, []);
+                grouped.set(forecast.roomEmail, {name: currRoomName, list: []});
             }
-            grouped.get(forecast.roomEmail)!.push(record);
+            grouped.get(forecast.roomEmail)!.list.push(record);
         });
 
         const result: RoomHourlyForecastDTO[] = Array.from(grouped.entries()).map(
             ([email, forecasts]) => ({
                 roomEmail: email,
-                forecasts,
+                roomName: forecasts.name,
+                forecasts: forecasts.list,
             })
         );
 
