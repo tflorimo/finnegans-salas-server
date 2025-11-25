9344bc938baa519942c02571a5fac3c5a69b7d1b
gusV15 <gustavov0706@gmail.com>
2025-11-24 22:44:58 -0300

    Reorganize route imports for clarity and consistency

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import authRoutes from './authRoutes';
-import roomRoutes from './roomRoutes';
 import eventRoutes from './eventRoutes';
+import forecastRoutes from './forecastRoutes';
+import roomRoutes from './roomRoutes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/rooms', roomRoutes);
 router.use('/events', eventRoutes);
+router.use('/forecast', forecastRoutes);
 
 export default router;
